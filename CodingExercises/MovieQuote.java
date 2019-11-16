ac1dbe08518b52d814273e64c81efa0635d17a0a
b3nwong <53283480+b3nwong@users.noreply.github.com>
2019-11-16 09:46:28 +0800

    practice qn 6 & 7

---
+++ CodingExercises/MovieQuote.java
@@ -0,0 +1,7 @@
+public class MovieQuote {
+    public static void main(String[] args) {
+        String quote = "I'll be back.";
+        System.out.println(quote);
+    }
+
+}
---
+++ CodingExercises/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        String quote = "Show me the money!";
+        String movie = "Jerry Maguire";
+        String character = "Jerry Maguire";
+        int year = 1996;
+
+        System.out.println(quote + "\n" + movie + "\n" + character + "\n" + year);
+    }
+}
