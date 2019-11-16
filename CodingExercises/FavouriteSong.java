2040f981d231de5505920e3a6f8d4f85fc983cdf
b3nwong <53283480+b3nwong@users.noreply.github.com>
2019-11-16 10:31:04 +0800

    practice qn 6 & 7

---
+++ CodingExercises/FavouriteSong.java
@@ -0,0 +1,11 @@
+public class FavouriteSong {
+    public static void main(String[] args)
+    {
+        String lineOne = "You get the best of both worlds";
+        String lineTwo = "Chill it out, take it slow";
+        String lineThree = "Then you rock out the show";
+        String lineFour = "You get the best of both worlds";
+
+        System.out.println(lineOne + "\n" + lineTwo + "\n" + lineThree + "\n" + lineFour);
+    }
+}
